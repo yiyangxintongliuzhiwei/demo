f039aa7178035cee93added5a7a887f1d56d1669
yiyangxintongliuzhiwei <1686327965@qq.com>
2018-09-21 18:07:33 +0800

    helloword界面提交

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,43 +1,9 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <!-- <heads msg="第一个项目" @messages="resive"> 
-    </heads> -->
-    <!-- <p>{{childword}}</p> -->
-    <!-- <echars></echars> -->   
-    <!-- <increment></increment>
-    <display></display> -->
     <router-view/>
   </div>
 </template>
 <script>  
-import heads from "./components/heads.vue"
-import display from "./components/display.vue"
-import increment from "./components/increment.vue"
-// import echars from "./components/echars.vue"
 export default {
-  name: 'App',
-  data(){
-    return{
-       childword:"你好",
-    }
-  },
- components:{heads,display,increment},
- methods:{
-  resive:function(){
-     this.childword = "子组件像父组件通信";
-  }
- }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- demo/src/components/HelloWorld.vue
+++ demo/src/components/HelloWorld.vue
@@ -1,121 +1,333 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <button @click="tongji()">
-      点击
-    </button>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+    <div class="zhuomian">
+      <div class="person1"></div>
+      <div class="person1-box">
+      <div v-if='checked[index]' v-for="(item,index) in arr1" class="puke" @click="select(index)" :class="{active:checked[index].flag}">
+        <div>{{item.textContent.split(' ')[0]}}</div>
+        <div>{{item.textContent.split(' ')[1]}}</div>
+      </div>
+      </div>
+      <div class="person2"></div>
+      <div class="person2-box">
+      <div v-for="item in arr2" id="puke">
+        <div>{{item.textContent.split(' ')[0]}}</div>
+        <div>{{item.textContent.split(' ')[1]}}</div>
+      </div>
+      </div>
+      <div class="person3"></div>
+      <div class="person3-box">
+      <div v-for="item in arr3" id="puke">
+        <div>{{item.textContent.split(' ')[0]}}</div>
+        <div>{{item.textContent.split(' ')[1]}}</div>
+      </div>
+      </div>
+      <div class="person4" v-show="!pukelist">底牌</div>
+      <div class="person4-box">
+      <div v-for="item in arr4" id="puke">
+        <div>{{item.textContent.split(' ')[0]}}</div>
+        <div>{{item.textContent.split(' ')[1]}}</div>
+      </div>
+      </div>
+      <div class="zhezhao" v-show="pukelist"></div>
+      <div class="pukelist" v-show="pukelist">
+        <div v-for="item in puke" id="puke">
+          <div>{{item.color}}</div>
+          <div>{{item.value}}</div>
+        </div>
+      </div>
+      <div class="chuledepai" v-show="chulaidepai">
+        <div v-for="item in zhuangpai" class="puke">
+          <div>{{item.textContent.split(' ')[0]}}</div>
+          <div>{{item.textContent.split(' ')[1]}}</div>
+        </div>
+      </div>
+      <button class="start" @click="start()" v-show="pukelist">发牌</button>
+      <button class="start" @click='jiaodizhu()' v-show="jiaodizhus">叫地主</button>
+      <button class="buchu" v-show="buchu">不出</button>
+      <button class="chupai" v-show="buchu" @click="chupaizou">出牌</button>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      arr1: [],
+      checked: [{ flag: false }],
+      buchu: false,
+      pukelist: true,
+      jiaodizhus: false,
+      chulaidepai: false,
+      zhuangpai: [],
+      arr2: [],
+      arr3: [],
+      arr4: [],
+      puke:[
+        {color:'hong', value: '1'},
+        {color:'hong', value: '2'},
+        {color:'hong', value: '3'},
+        {color:'hong', value: '4'},
+        {color:'hong', value: '5'},
+        {color:'hong', value: '6'},
+        {color:'hong', value: '7'},
+        {color:'hong', value: '8'},
+        {color:'hong', value: '9'},
+        {color:'hong', value: '10'},
+        {color:'hong', value: '11'},
+        {color:'hong', value: '12'},
+        {color:'hong', value: '13'}
+      ]
     }
   },
-  methods:{
-    tongji:function(){
-      this.$root.bus.$emit("change","haha")
+  methods: {
+    // 发牌
+    start() {
+      var puke = document.querySelectorAll('#puke')
+      var arr1in = []
+      var arr2in = []
+      var arr3in = []
+      var arr4in = []
+      var newpuke = []
+      //每个人的牌和底牌分别放在四个数组里
+      for (let i = 0; i < 4; i++) {
+        arr1in.push(Math.floor(Math.random()*puke.length))
+        arr2in.push(Math.floor(Math.random()*puke.length))
+        arr3in.push(Math.floor(Math.random()*puke.length))
+      }
+      arr4in.push(Math.floor(Math.random()*puke.length))
+      for (let j = 0; j < arr1in.length; j++) {
+        this.arr1.push(puke[arr1in[j]])
+      }
+      this.arr1.forEach(element => {
+        this.checked.push({ flag: false })
+      })
+      for (let j = 0; j < arr2in.length; j++) {
+        this.arr2.push(puke[arr2in[j]])
+      }
+      for (let j = 0; j < arr1in.length; j++) {
+        this.arr3.push(puke[arr3in[j]])
+      }
+      this.arr4.push(puke[arr4in[0]])
+      this.pukelist = false
+      this.jiaodizhus = true
+    },
+    //本方选择牌
+    select(index) {
+      this.checked[index].flag = !this.checked[index].flag
+    },
+    //叫地主
+    jiaodizhu() {
+      this.jiaodizhus = false
+      this.arr1.push(this.arr4[0])
+      this.buchu = true
+    },
+    //出牌
+    chupaizou() {
+      this.zhuangpai = []
+      this.chulaidepai = true
+      for (let i = 0; i < this.checked.length; i++) {
+        if (this.checked[i].flag) {
+          this.zhuangpai.push(this.arr1[i])
+        }
+      }
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss">
+  .zhuomian{
+    width: 740px;
+    height: 490px;
+    background: skyblue;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    margin-left: -370px;
+    margin-top: -245px;
+    .start{
+      width: 80px;
+      height: 20px;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      margin-left: -25px;
+      margin-top: -60px;
+    }
+    .buchu{
+      width: 80px;
+      height: 20px;
+      position: absolute;
+      left: 55%;
+      top: 50%;
+      // margin-left: -25px;
+      // margin-top: -60px;
+    }
+    .chupai{
+      width: 80px;
+      height: 20px;
+      position: absolute;
+      left: 35%;
+      top: 50%;
+    }
+    .zhezhao{
+      width:50px;
+      height: 80px;
+      // border: 1px solid #000;
+      background: mediumaquamarine;
+      z-index: 2;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      margin-left: -25px;
+      margin-top: -180px;
+    }
+    .pukelist{
+      width:50px;
+      height: 80px;
+      // border: 1px solid #000;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      margin-left: -25px;
+      margin-top: -180px;
+      #puke{
+        width:50px;
+        height: 80px;
+        // border: 1px solid #000;
+        position: absolute;
+        left: 0;
+        top: 0;
+        background: mediumaquamarine
+      }
+    }
+    .chuledepai{
+      width: 350px;
+      height: 80px;
+      border: 1px solid #000;
+      position: absolute;
+      left: 230px;
+      bottom: 90px;
+      .puke{
+        width:50px;
+        height: 80px;
+        border: 1px solid #000;
+        cursor: pointer;
+        // position: absolute;
+        // left: 0;
+        // top: 0;
+        margin-right: -20px;
+        float: left;
+        background: mediumaquamarine
+      }
+    }
+    .person1{
+      width: 50px;
+      height: 50px;
+      border: 1px solid #000;
+      position: absolute;
+      left: 100px;
+      bottom: 10px;
+    }
+    .person1-box{
+      width: 500px;
+      height: 80px;
+      border: 1px solid #000;
+      position: absolute;
+      left: 160px;
+      bottom: 10px;
+      .puke{
+        width:50px;
+        height: 80px;
+        border: 1px solid #000;
+        cursor: pointer;
+        // position: absolute;
+        // left: 0;
+        // top: 0;
+        margin-right: -20px;
+        float: left;
+        background: mediumaquamarine
+      }
+       .active{
+        transform: translateY(-10px);
+      }
+    }
+    .person2{
+      width: 50px;
+      height: 50px;
+      border: 1px solid #000;
+      position: absolute;
+      left: 10px;
+      top: 100px;
+    }
+    .person2-box{
+      width: 50px;
+      height: 80px;
+      border: 1px solid #000;
+      position: absolute;
+      left: 10px;
+      top: 160px;
+      #puke{
+        width:50px;
+        height: 80px;
+        border: 1px solid #000;
+        position: absolute;
+        left: 0;
+        top: 0;
+        background: mediumaquamarine
+      }
+    }
+    .person3{
+      width: 50px;
+      height: 50px;
+      border: 1px solid #000;
+      position: absolute;
+      right: 10px;
+      top: 100px;
+    }
+    .person3-box{
+      width: 50px;
+      height: 80px;
+      border: 1px solid #000;
+      position: absolute;
+      right: 10px;
+      top: 160px;
+      #puke{
+        width:50px;
+        height: 80px;
+        border: 1px solid #000;
+        position: absolute;
+        left: 0;
+        top: 0;
+        background: mediumaquamarine
+      }
+    }
+    .person4{
+      width: 35px;
+      height: 30px;
+      border: 1px solid #000;
+      position: absolute;
+      right: 280px;
+      top: 20px;
+    }
+    .person4-box{
+      width: 50px;
+      height: 80px;
+      // border: 1px solid #000;
+      position: absolute;
+      left: 346px;
+      top: 20px;
+      #puke{
+        width:50px;
+        height: 80px;
+        // border: 1px solid #000;
+        position: absolute;
+        left: 0;
+        top: 0;
+        background: mediumaquamarine
+      }
+    }
+  }
 </style>
+
